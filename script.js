183c278e3f3a7f8548cc832ec5a8a8035a3c8b34
AhmedAlaa27 <ahmedalaa277143@gmail.com>
2024-08-05 14:44:17 +0300

    Create a function that gets user choice

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    
+
     choice = Math.floor(Math.random() * 3)
 
     if (choice == 0) return "Rock"
@@ -8,3 +8,12 @@ function getComputerChoice() {
 
 }
 
+function getHumanChoice() {
+    
+    let choice = parseInt(prompt("1. Rock\n2. Paper\n3. Scissors\n"))
+
+    if (choice == 1) return "Rock"
+    else if (choice == 2) return "Paper"
+    else if (choice == 3) return "Scissors"
+
+}
